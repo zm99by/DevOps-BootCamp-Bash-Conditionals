7b6dc23a9a5d00fdb81341fb27d929d19fe1a313
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-07-18 23:53:48 +0200

    add temp task

--- temp.sh
+++ temp.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+Kelvins="K"
+Celsius="C"
 
-# Place your code here
+K=$(echo $1 | grep "K")
+if [ $? -eq 0 ]
+then
+  rez=$(echo $K | tr -d "K"); echo $(($rez-273))$Celsius
+fi
+
+C=$(echo $1 | grep "C")
+if [ $? -eq 0 ]
+then
+  rez=$(echo $C | tr -d "C"); echo $(($rez+273))$Kelvins
+fi
