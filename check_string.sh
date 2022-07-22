7615c09e949d9bc034eca2269460131325c1494d
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-07-22 20:44:39 +0200

    add task3

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#set -x
 
-# Place your code here
+count=$(echo $1 | tr -d '[*!@#$%^&()_+.]'[:lower:][:upper:][:space:] | wc -c)
+symb=$(echo $1 | tr -d [:digit:][:lower:][:upper:][:space:] | wc -c)
+leter=$(echo $1 | tr -d '[*!@#$%^&()_+.]'[:digit:][:space:] | wc -c)
+
+echo Numbers: $count Symbols: $symb Letters: $leter
